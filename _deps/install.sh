291cbd167b4469208347ae0f15becd8b3e64b73e
2144 <719577252@qq.com>
2024-10-17 19:46:21 +0800

    ss

---
+++ _deps/install.sh
@@ -0,0 +1,11 @@
+
+git clone git@gitee.com:MkJzCode/glm.git 
+git clone git@gitee.com:yuanji1020/yaml-cpp.git 
+git clone git@gitee.com:fghydx-osc_admin/reactphysics3d.git 
+git clone git@gitee.com:jimodesaonian/freetype.git
+git clone git@gitee.com:colorcolor/glfw.git
+git clone git@gitee.com:fwy000/assimp.git
+git clone git@gitee.com:magicor/curl.git
+git clone git@gitee.com:xeyon/imnodes.git
+git clone git@gitee.com:sskey/openal-soft.git
+git clone -b docking git@gitee.com:zhouxs1023/imgui.git
