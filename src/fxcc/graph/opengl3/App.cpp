919b6d424d2a8298fe1472af3ac1eff5ac38f1fb
2144 <719577252@qq.com>
2024-10-20 21:16:22 +0800

    ss

--- src/fxcc/graph/opengl3/App.cpp
+++ src/fxcc/graph/opengl3/App.cpp
@@ -8,25 +8,34 @@ namespace
 {
 	enum DepthStencilType
 	{
-		_dp_none_,
+		_dp_none_ = 0,
 
 		// depth less and no stencil 
-		_dp_dlns_,
+		_dp_depth_less_stencil_no_,
 
 		// depth less and stencil always
-		_dp_dlas_,
+		_dp_depth_less_stencil_always_,
+	};
+	enum BlendType
+	{
+		_b_none_ = 0,
+
+		// blend render font
+		_b_font_,
 	};
 
 
 	struct RenderStateDesc
 	{
+		bool m_ClearColor;
+		bool m_ClearDepth;
+		bool m_ClearStencil;
+
 		glm::vec4 m_BackgroudColor;
 		int m_DepthValue;
 		int m_StencilValue;
 
-		bool m_ClearColor;
-		bool m_ClearDepth;
-		bool m_ClearStencil;
+		DepthStencilType m_DepthStencilType;
 
 		RenderStateDesc() :
 			m_ClearColor(true), 
@@ -41,18 +50,83 @@ namespace
 		};
 	};
 
+
 	RenderStateDesc m_RenderStateDesc;
 	struct DepthStencilDesc
 	{
 		bool m_EnableDepthFunc;
+		bool m_EnableStencilFunc;
+
 		int m_DepthFunc;
 
-		DepthStencilDesc() :m_EnableDepthFunc(true), m_DepthFunc(GL_LESS)
+		int m_StencilFunc;
+		int m_StencilRef;
+		int m_StencilMask;
+
+		int m_StencilWriteMask;
+		int m_StencilFail, m_DepthFail, m_DepthStencilPass;
+
+		DepthStencilDesc() :m_EnableDepthFunc(true), m_DepthFunc(GL_LESS), m_EnableStencilFunc(false)
 		{
 		}
 	};
+
+	struct BlendDesc
+	{
+		bool m_EnableBlend;
+		int m_Sfactor, m_Dfactor;
+	};
+	struct RasterizerDesc {
+		int m_PolyMode;
+		int m_PolyFillMode;
+		bool m_EnableCullface;
+		int m_Cullface;
+		int m_FrontFace;
+
+		RasterizerDesc()
+			:
+			m_PolyMode(GL_FRONT_AND_BACK),
+			m_PolyFillMode(GL_FILL),
+			m_EnableCullface(true),
+			m_Cullface(GL_BACK),
+			m_FrontFace(GL_CCW)
+		{
+
+		}
+	
+	};
+
 	struct RenderStatus
 	{
+
+		static void Clear(const RasterizerDesc& desc)
+		{
+			glPolygonMode(desc.m_PolyMode, desc.m_PolyFillMode);
+
+			if (desc.m_EnableCullface)
+			{
+				glEnable(GL_CULL_FACE);
+				glCullFace(desc.m_Cullface);
+				glFrontFace(desc.m_FrontFace);
+			}
+			else
+			{
+				glDisable(GL_CULL_FACE);
+			}
+
+		}
+		static void Clear(const BlendDesc& desc)
+		{
+			if (desc.m_EnableBlend)
+			{
+				glEnable(GL_BLEND);
+				glBlendFunc(desc.m_Sfactor, desc.m_Dfactor);
+			}
+			else
+			{
+				glDisable(GL_BLEND);
+			}
+		};
 		static void Clear(const DepthStencilDesc& desc)
 		{
 			if (desc.m_EnableDepthFunc) {
@@ -62,6 +136,18 @@ namespace
 			else {
 				glDisable(GL_DEPTH_TEST);
 			}
+			if (desc.m_EnableStencilFunc)
+			{
+				glEnable(GL_STENCIL_TEST);
+				glStencilFunc(desc.m_StencilFunc, desc.m_StencilRef, desc.m_StencilMask);
+				glStencilMask(desc.m_StencilWriteMask);
+				glStencilOp(desc.m_StencilFail, desc.m_DepthFail, desc.m_DepthStencilPass);
+			}
+			else
+			{
+				glDisable(GL_STENCIL_TEST);
+			}
+
 		}
 		static void Clear(const RenderStateDesc& desc)
 		{
